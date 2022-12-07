1d478504142f50f6d789571943555468bc76187d
berkay.ozdemir <berkay.ozdemir@etiya.com>
2022-12-07 10:57:19 +0300

    feat : adding base view model and builders

---
+++ lib/src/view_models/base_view_models.dart
@@ -0,0 +1,142 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
+
+
+import '../helpers/index.dart';
+import '../reactive_services/index.dart';
+
+/// Contains ViewModel functionality for busy and error state management
+class BaseViewModel extends ChangeNotifier
+    with BuilderHelpers, ErrorStateHelper {
+  @override
+  void notifyListeners() {
+    if (!disposed) {
+      super.notifyListeners();
+    }
+  }
+
+  @override
+  void dispose() {
+    disposed = true;
+    super.dispose();
+  }
+}
+
+/// A [BaseViewModel] that provides functionality to subscribe to a reactive service.
+abstract class ReactiveViewModel extends BaseViewModel {
+  late List<ReactiveServiceMixin> _reactiveServices;
+
+  List<ReactiveServiceMixin> get reactiveServices;
+
+  ReactiveViewModel() {
+    _reactToServices(reactiveServices);
+  }
+
+  void _reactToServices(List<ReactiveServiceMixin> reactiveServices) {
+    _reactiveServices = reactiveServices;
+    for (var reactiveService in _reactiveServices) {
+      reactiveService.addListener(_indicateChange);
+    }
+  }
+
+  @override
+  void dispose() {
+    for (var reactiveService in _reactiveServices) {
+      reactiveService.removeListener(_indicateChange);
+    }
+    super.dispose();
+  }
+
+  void _indicateChange() {
+    notifyListeners();
+  }
+}
+
+@protected
+class DynamicSourceViewModel<T> extends ReactiveViewModel {
+  bool changeSource = false;
+  void notifySourceChanged() {
+    changeSource = true;
+  }
+
+  @override
+  List<ReactiveServiceMixin> get reactiveServices => [];
+}
+
+/// Interface: Additional actions that should be implemented by spcialised ViewModels
+abstract class Initialisable {
+  void initialise();
+}
+
+class StreamData<T> extends DynamicSourceViewModel<T>
+    with MessageStateHelper, DataStateHelper<T> {
+  Stream<T> stream;
+
+  /// Called when the new data arrives
+  ///
+  /// notifyListeners is called before this so no need to call in here unless you're
+  /// running additional logic and setting a separate value.
+  Function? onData;
+
+  /// Called after the stream has been listened too
+  Function? onSubscribed;
+
+  /// Called when an error is placed on the stream
+  Function? onError;
+
+  /// Called when the stream is cancelled
+  Function? onCancel;
+
+  /// Allows you to modify the data before it's set as the new data for the ViewModel
+  ///
+  /// This can be used to modify the data if required. If nothhing is returned the data
+  /// won't be set.
+  Function? transformData;
+  StreamData(
+    this.stream, {
+    this.onData,
+    this.onSubscribed,
+    this.onError,
+    this.onCancel,
+    this.transformData,
+  });
+  late StreamSubscription _streamSubscription;
+
+  void initialise() {
+    _streamSubscription = stream.listen(
+      (incomingData) {
+        setError(null);
+        setMessage(null);
+        // Extra security in case transformData isnt sent
+        var interceptedData =
+            transformData == null ? incomingData : transformData!(incomingData);
+
+        if (interceptedData != null) {
+          data = interceptedData;
+        } else {
+          data = incomingData;
+        }
+
+        notifyListeners();
+        onData!(data);
+      },
+      onError: (error) {
+        setError(error);
+        data = null;
+        onError!(error);
+        notifyListeners();
+      },
+    );
+
+    onSubscribed!();
+  }
+
+  @override
+  void dispose() {
+    _streamSubscription.cancel();
+    onCancel!();
+
+    super.dispose();
+  }
+}
